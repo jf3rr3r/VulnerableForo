14d226d2eef5cbe77863a8d7886bd8f3a5893be5
jf3rr3r <jaimeferrer3@gmail.com>
2021-05-01 17:23:31 +0200

    Upload: Subida de archivos del foro - Sin reparar

---
+++ web/public/config/config.php
@@ -0,0 +1,12 @@
+<?php
+//DB Params
+define ("DB_HOST", "mysqldb");
+define ("DB_USER", "root");
+define ("DB_PASS", "root");
+define ("DB_NAME", "foro");
+
+define("SITE_TITLE", "Bienvenido al foro!");
+
+//Paths
+define('BASE_URI','http://'.$_SERVER['SERVER_NAME'].':8000/');
+
